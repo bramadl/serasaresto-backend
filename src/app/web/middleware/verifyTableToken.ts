21e7d3e0de7b248e6d298a77d67477394a2556dc
bramadl <thenamesbram@gmail.com>
2022-06-10 16:13:22 +0700

    feat: middleware for verifying the users table

---
+++ src/app/web/middleware/verifyTableToken.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import { TableToken } from "../../../modules/customers/domains/valueObjects/TableToken";
+import { tableRepository } from "../../../modules/customers/repositories";
+
+export async function verifyTableToken(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const headers = req.headers.authorization;
+  if (!headers) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized, please provide a valid table token." });
+  }
+
+  const [bearer, token] = headers.split(" ");
+  if (bearer !== "Bearer") {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized, please use a valid bearer type." });
+  }
+
+  const createToken = TableToken.create({ value: token });
+  const tableRepo = await tableRepository.findByTableToken(
+    createToken.getValue()
+  );
+
+  const table = tableRepo.getValue();
+
+  if (table.token.value === token) {
+    next();
+  } else {
+    return res
+      .status(403)
+      .json({ message: "Meja ini sedang direservasi orang lain." });
+  }
+}
