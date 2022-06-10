e045656fcf5337da7c7372e316566e713d401a27
bramadl <thenamesbram@gmail.com>
2022-06-10 16:17:37 +0700

    chore: use verifyTable middleware on menu endpoints

--- src/app/web/routes/api/menus.api.ts
+++ src/app/web/routes/api/menus.api.ts
@@ -1,9 +1,10 @@
 import { Request, Response, Router } from "express";
 
 import { getMenusController } from "../../../../modules/orders/controllers";
+import { verifyTableToken } from "../../middleware/verifyTableToken";
 
 export const menuRouter = Router();
 
-menuRouter.get("/list", (req: Request, res: Response) => {
+menuRouter.get("/list", verifyTableToken, (req: Request, res: Response) => {
   return getMenusController.execute(req, res);
 });
